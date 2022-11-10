f772b92c830ab36cd2227d1c277fb52667ed2ae1
param-ninthu <ninthu1999@gmail.com>
2022-11-10 09:45:13 +0530

    login

--- official_vbuyy/lib/screens/common/login.dart
+++ official_vbuyy/lib/screens/common/login.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -11,6 +13,63 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          color: Colors.white,
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.topCenter,
+                margin: EdgeInsets.all(20),
+                height: 200,
+                width: 200,
+                child: Image.asset('assets/images/login.png'),
+              ),
+              Container(
+                margin: EdgeInsets.all(15),
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Login',
+                  style: GoogleFonts.poppins(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.blue),
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(15, 5, 0, 0),
+                      alignment: Alignment.topLeft,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Email ID',
+                          prefixIcon: Icon(Icons.email_rounded),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(15, 5, 0, 0),
+                      alignment: Alignment.topLeft,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Password',
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
+//1F67F1
