f8488626aed2b8f483e1f6d59e2a5098f76db40b
param-ninthu <ninthu1999@gmail.com>
2023-06-07 00:15:25 +0530

    rough header

--- lib/screens/client/home.dart
+++ lib/screens/client/home.dart
@@ -14,7 +14,36 @@ class Home extends StatelessWidget {
           height: MediaQuery.of(context).size.height * 0.85,
           color: HexColor("#fff"),
           width: MediaQuery.of(context).size.width,
-          child: Expanded(child: Text("Home")),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                height: MediaQuery.of(context).size.height * 0.08,
+                width: MediaQuery.of(context).size.width,
+                color: HexColor("#fff"),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: Text(
+                        "Hi Kei",
+                        style: TextStyle(
+                            color: HexColor("#000"),
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: HexColor("#000"), shape: BoxShape.circle),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
